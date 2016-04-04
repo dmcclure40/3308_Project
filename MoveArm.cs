36a3783755e3f20f4c10a98d48554d48f1e91bdd
Davis McClure <dmcclure40@gmail.com>
2016-04-04 14:58:31 -0600

    Initial code for robot arm movement (incomplete)

---
+++ MoveArm.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveArm : MonoBehaviour {
+
+    public Rigidbody rb;
+    float x = 0.2f;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+            /// print("the up arrow was pressed");
+            MoveArmUp();
+       
+	}
+
+    void MoveArmUp()
+    {
+        
+
+    }
+}
