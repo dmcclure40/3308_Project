9eed49d92b2e44749ca14ebfc40b1054f5c2e782
Davis McClure <dmcclure40@gmail.com>
2016-04-04 17:45:59 -0600

    Joint and Arm script for Movement

---
+++ JointScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JointScript : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    float force = 0.05f;
+    float NewForce = 0.9f;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            MoveJointDown();
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            MoveJointUp();
+
+
+
+    }
+
+    void MoveJointDown()
+    {
+        print("a key was pressed");
+        NewForce -= force;
+        print("force = " + NewForce);
+        transform.position = new Vector3(transform.position.x, NewForce, transform.position.z);
+
+    }
+
+    void MoveJointUp()
+    {
+        print("a key was pressed");
+        NewForce += force;
+        print("force = " + NewForce);
+        transform.position = new Vector3(transform.position.x, NewForce, transform.position.z);
+
+    }
+}
--- MoveArm.cs
+++ MoveArm.cs
@@ -4,23 +4,40 @@ using System.Collections;
 public class MoveArm : MonoBehaviour {
 
     public Rigidbody rb;
-    float x = 0.2f;
+
+    float force = 5f;
+    float NewForce = 0.85f;
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyUp(KeyCode.UpArrow))
-            /// print("the up arrow was pressed");
-            MoveArmUp();
-       
-	}
+	void FixedUpdate () {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            MoveArmDown();
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            MoveArmUp(); 
+
+
+
+    }
+
+    void MoveArmDown()
+    {
+        print("a key was pressed");
+        //NewForce -= force;
+        print("force = " + NewForce);
+        transform.Rotate(Vector3.forward, -force * Time.deltaTime, Space.Self);
+
+    }
 
     void MoveArmUp()
     {
-        
+        print("a key was pressed");
+        //NewForce += force;
+        print("force = " + NewForce);
+        transform.Rotate(Vector3.forward, force *Time.deltaTime, Space.Self);
 
     }
 }
