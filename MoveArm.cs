ac410aebe17634e7ee15c3cb88e531660a41881e
Davis McClure <dmcclure40@gmail.com>
2016-04-25 11:04:58 -0600

    updated move arm to change when Y changes

--- MoveArm.cs
+++ MoveArm.cs
@@ -1,25 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class MoveArm : MonoBehaviour {
 
     public Rigidbody rb;
     public float RotateSpeed = 50f;
-    
+    public TextAsset textFile;
+    public float Y = 300f;
+    public float tempY = 100;
+
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
+        //string text = textFile.text;
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        //print(textFile);
+         if (Y > tempY)
         {
+            print(Y);
+            print(tempY);
             MoveArmDown();
+            Y = tempY;
+            print(Y);
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if(Y < tempY)
         {
             MoveArmUp();
+            Y = tempY;
         }
     }
 
