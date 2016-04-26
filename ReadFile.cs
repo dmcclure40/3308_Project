b0a31486673577fe00e10e238e2641d4a2300a8d
Ninjahamster611 <ninjahamster94@gmail.com>
2016-04-26 15:51:48 -0600

    Added files via upload

---
+++ ReadFile.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+using System.IO;
+
+public class ReadFile : MonoBehaviour
+{
+
+
+    FileStream fs = new FileStream("input.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+    StreamReader sr;
+
+
+
+    public ReadFile()
+    {
+
+        using (StreamReader sr = new StreamReader(fs))
+        {
+
+            string line;
+            int currentLineNumber = 0;
+            decimal xpos = 0;
+            //decimal xposnew = 0;
+            decimal ypos = 0;
+            //decimal yposnew = 0;
+
+            while (!sr.EndOfStream)
+            {
+                currentLineNumber++;
+                line = sr.ReadLine();
+
+                // Debug.Log(line);
+
+                try
+                {
+                    switch (currentLineNumber)
+                    {
+                        case 1:
+                            xpos = decimal.Parse(line);
+                            Debug.Log("xpos = " + xpos);
+                            break;
+                        case 2:
+                            ypos = decimal.Parse(line);
+                            Debug.Log("ypos = " + ypos);
+                            break;
+                    }
+
+
+
+                }
+
+                catch (Exception ex)
+                {
+                    Debug.Log("Error: " + ex.Message + ". Exiting.");
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ XYDemo.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class XYDemo : MonoBehaviour
+{
+    // Change input.x and input.y in inspector
+    // Observe output.x and output.y in inspector
+    //public Vector2 input;
+    public Vector2 output;
+    public TextAsset textFile;
+
+   // XYStreamWriter writer = XYStreamWriter.FromFile("data.txt");
+    XYStreamReader reader = XYStreamReader.FromFile("input.txt");
+
+    void Start()
+    {
+        // Intentionally not reading/writing to disk
+        // too often because I care about my hard drive.
+        //InvokeRepeating("Write", 0.5f, 1.0f);
+        InvokeRepeating("Read", 1.0f, 1.0f);
+    }
+
+    void OnDestroy()
+    {
+        //writer.Dispose();
+        reader.Dispose();
+    }
+
+    void Read()
+    {
+        print("Read...");
+        reader.Read();
+        print(output);
+        output = reader.XY;
+    }
+
+    //void Write()
+    //{
+       //print("Write...");
+       //writer.XY = input;
+       //writer.Write();
+   // }
+}
